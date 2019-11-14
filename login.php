63ae439699742dd2b8b07bb466adc16370a5ee03
jmsilva <jmsilva@impresa.pt>
2019-11-14 18:27:48 +0000

    Fix issue with login form action

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <div class="container">
     <div class="row text-center">
         <div class="col-md-5 mx-auto">
-            <form class="form-signin" action="<?php echo $_SERVER[PHP_SELF]; ?>" method="post">
+            <form class="form-signin" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                 <h1 class="h3 mb-3 font-weight-normal">Please login</h1>
                 <label for="inputEmail" class="sr-only">Email address</label>
                 <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
