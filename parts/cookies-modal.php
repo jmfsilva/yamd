fcfca454c48afd76adc27a779126953ce5ecb6b2
João Silva <joaosilva@Joaos-MacBook-Pro.local>
2020-02-09 21:01:30 +0000

    Add cookies modal and stub scripts.js

---
+++ parts/cookies-modal.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="cookiesModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Este site usa cookies</h5>
+      </div>
+      <div class="modal-body">
+        <p>O Yet Another Music Database e os seus parceiros utilizam Cookies para manter informação do visitante, permitindo determinar as suas preferências, auxiliar no preenchimento de formulários, permitir o acesso a áreas privadas do website onde seja necessária autenticação, bem como recolher indicadores de performance, origem e horário dos acessos ao website.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button>
+      </div>
+    </div>
+  </div>
+</div>
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+
+function getCookie(cname) {
+    return true;
+}
+
+function setCookie(cname, cvalue, exdays) {
+}
+
+$('#cookiesModal')
+.on('hide.bs.modal', function (e) {
+    setCookie("CONSENT", true, 1000);
+}).modal({
+    show: !getCookie("CONSENT")
+})
